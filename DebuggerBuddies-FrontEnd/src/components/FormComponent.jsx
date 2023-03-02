dcc22152a01af0ea5137117966afd8f1e31e317d
Leo D. Penrose <lpenrose@depaul.edu>
2023-03-02 00:06:49 -0600

    updated form component to read form data

--- DebuggerBuddies-FrontEnd/src/components/FormComponent.jsx
+++ DebuggerBuddies-FrontEnd/src/components/FormComponent.jsx
@@ -1,6 +1,18 @@
 import "../styles/index.css"
+import {useState} from "react";
 
 export default function FormComponent(props){
+    const [formData,setFormData] = useState({firstName:"",lastName:"",email:"",password:"",role:""})
+    console.log(formData)
+
+    function hello(event){
+        setFormData(prevState => {
+            return {
+                ...prevState,[event.target.name]:event.target.value
+            }
+        })
+    }
+
     return(
 
 
@@ -8,11 +20,11 @@ export default function FormComponent(props){
 
                 <h2>{props.formType}</h2>
                 <form className={"admin-input"} action="">
-                    <input type="text" placeholder={"First Name"}/>
-                    <input type="text" placeholder={"Last Name"}/>
-                    <input type="email" placeholder={"Email"}/>
-                    <input type="text" placeholder={"Password"}/>
-                    <input type="text" placeholder={"Role"}/>
+                    <input name={"firstName"} onChange={hello} type="text" placeholder={"First Name"}/>
+                    <input name={"lastName"} onChange={hello} type="text" placeholder={"Last Name"}/>
+                    <input name={"email"} onChange={hello} type="email" placeholder={"Email"}/>
+                    <input name={"password"} onChange={hello} type="text" placeholder={"Password"}/>
+                    <input name={"role"} onChange={hello} type="text" placeholder={"Role"}/>
                 </form>
 
                 <button type="button" className={`btn btn-${props.btnType}`}>{props.btnTxt}</button>
